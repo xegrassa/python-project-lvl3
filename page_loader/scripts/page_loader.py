165194407ea3d2955b4e13823780e869e1b9e6f8
egrassa <bacek90@mail.ru>
2020-03-09 21:31:46 +0300

    Auto Commit

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,5 +1,21 @@
+import argparse
+import os.path
+import requests
+
+
+def get_html(URL):
+    r = requests.get(URL)
+    return r
+
+
+def gen_file_name(URL):
+    
 def main():
-    print('TEST')
+    parser= argparse.ArgumentParser()
+    parser.add_argument('URL', type=str, help='URL')
+    parser.add_argument('--output', type=str, help='path to download page', default='/')
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
