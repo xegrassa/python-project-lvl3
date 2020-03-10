4f533dc13ede2f5681e701d3d2a81e9970aa9c61
egrassa <bacek90@mail.ru>
2020-03-10 21:59:16 +0300

    Auto Commit

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,21 +1,38 @@
 import argparse
+import os
 import os.path
 import requests
+import re
 
 
 def get_html(URL):
     r = requests.get(URL)
-    return r
+    return r.text
 
 
 def gen_file_name(URL):
-    
+    parts_file_name = re.findall('[^\W]+', URL)
+    file_name = '-'.join(parts_file_name[1:]) + '.html'
+    return file_name
+
+
 def main():
-    parser= argparse.ArgumentParser()
-    parser.add_argument('URL', type=str, help='URL')
-    parser.add_argument('--output', type=str, help='path to download page', default='/')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('URL',
+                        type=str,
+                        help='URL')
+    parser.add_argument('--output',
+                        type=str,
+                        help='path to download page',
+                        default=os.getcwd())
     args = parser.parse_args()
-    print(args)
+    path_file = os.path.join(args.output, gen_file_name(args.URL))
+    with open(path_file, 'w', encoding='utf-8') as file:
+        file.write(get_html(args.URL))
+
+    # print(os.path.join(args.output, gen_file_name(args.URL)))
+    # print(args)
+    # print(gen_file_name(args.URL))
 
 
 if __name__ == '__main__':
