b89567da0e904b0bd321ead6f222df7e256e5e83
egrassa <bacek90@mail.ru>
2020-03-22 12:51:02 +0300

    Auto Commit

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,14 +1,14 @@
 from page_loader.scripts.page_loader import get_html
-from page_loader.scripts.page_loader import gen_file_name
+from page_loader.scripts.page_loader import gen_name_file
 import tempfile
 
 
-SITES = "https://hexlet.io/courses"
+SITES = "hexlet.io/courses"
 
 
 def test_get_html():
 	pass
 	# assert get_html(SITES) == open('tests/fixtures/sites_html', 'r').read().rstrip()
 
-def test_gen_file_name():
-	assert gen_file_name(SITES) == open('tests/fixtures/sites_name', 'r').read().rstrip()
+def test_gen_name_file():
+	assert gen_name_file(SITES) == open('tests/fixtures/sites_name', 'r').read().rstrip()
