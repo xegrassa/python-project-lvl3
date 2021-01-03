e82083eca5c5b605872988c5a219b9d12a79f858
Egrassa <bacek90@mail.ru>
2021-01-03 17:23:54 +0500

    Remove test_case_data

--- tests/test_naming.py
+++ tests/test_naming.py
@@ -3,28 +3,30 @@ import pytest
 from page_loader.naming import (make_name_for_dir_files, make_name_from_url,
                                 replace_symbols)
 
-FIXTURE_NAME_FROM_URL = [('http://e1.ru', 'e1-ru.html'),
-                         ('http://avito.ru', 'avito-ru.html'),
-                         ('https://hex.io/courses', 'hex-io-courses.html')]
-FIXTURE_NAME_FOR_DIR_FILES = [('http://e1.ru', 'e1-ru_files'),
-                              ('http://avito.ru', 'avito-ru_files'),
-                              ('https://hex.io/course', 'hex-io-course_files')]
-FIXTURE_REPLACE_SYMBOLS = [('https://e1.ru', 'https-e1-ru'),
-                           ('http://avito.ru', 'http-avito-ru'),
-                           ('hexlet.io/courses', 'hexlet-io-courses'),
-                           ('hex.io/image.png', 'hex-io-image-png')]
 
-
-@pytest.mark.parametrize('url, expected_name', FIXTURE_NAME_FROM_URL)
+@pytest.mark.parametrize('url, expected_name', [
+    ('http://e1.ru', 'e1-ru.html'),
+    ('http://avito.ru', 'avito-ru.html'),
+    ('https://hex.io/courses', 'hex-io-courses.html')
+])
 def test_make_name_from_url(url, expected_name):
     assert make_name_from_url(url) == expected_name
 
 
-@pytest.mark.parametrize('url, expected_name', FIXTURE_NAME_FOR_DIR_FILES)
+@pytest.mark.parametrize('url, expected_name', [
+    ('http://e1.ru', 'e1-ru_files'),
+    ('http://avito.ru', 'avito-ru_files'),
+    ('https://hex.io/course', 'hex-io-course_files')
+])
 def test_make_name_for_dir_files(url, expected_name):
     assert make_name_for_dir_files(url) == expected_name
 
 
-@pytest.mark.parametrize('url, expected_name', FIXTURE_REPLACE_SYMBOLS)
+@pytest.mark.parametrize('url, expected_name', [
+    ('https://e1.ru', 'https-e1-ru'),
+    ('http://avito.ru', 'http-avito-ru'),
+    ('hexlet.io/courses', 'hexlet-io-courses'),
+    ('hex.io/image.png', 'hex-io-image-png')
+])
 def test_replace_symbols(url, expected_name):
     assert replace_symbols(url) == expected_name
